061147cf46861fe7021aeafe82a5da0aca669581
Mataya <taylucero2009@gmail.com>
2023-12-12 15:04:43 -0700

    sound function in progress

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ console.log('hi')
 
 const imgbackbtnel = document.getElementById('img-back');
 let arrayOfHiddenImages = [];
+
+const ftFoxySound = new Audio('./audio/ENCORE.ogg')
+const mangleSound = new Audio('./audio/mangle-6.ogg')
+const Roxysound = new Audio('./RN_ROXY_00029_pref.wav')
+
 function imagesback() {
     console.log('button was clicked!')
     arrayOfHiddenImages.map((imageID) => {
         document.getElementById(imageID).style.display = 'block'
-
     })
 }
 
@@ -19,4 +23,16 @@ function hideObject(imageID) {
         imgbackbtnel.style.display = 'block'
         arrayOfHiddenImages = []
     }
+}
+
+function voiceline(id) {
+    const idMatch = {
+        'ft-foxy': ftFoxySound,
+        'mangle': mangleSound,
+        'Roxy': RoxySound
+
+    }
+
+    const soundMatch = idMatch[id]
+    soundMatch && soundMatch.play()
 }
